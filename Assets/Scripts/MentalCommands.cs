39b10d339fd59c6668f0e0600cd2246e7593a765
Stoyan Peev <114337044+sspeev@users.noreply.github.com>
2023-12-07 09:33:34 +0200

    жхггк

--- Assets/Scripts/MentalCommands.cs
+++ Assets/Scripts/MentalCommands.cs
@@ -1,8 +1,6 @@
 using EmotivUnityPlugin;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class MentalCommands : MonoBehaviour
 {
