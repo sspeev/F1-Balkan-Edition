d0f6b98b283b69014dab5da14f4cfadb9045691e
Stoyan Peev <114337044+sspeev@users.noreply.github.com>
2023-12-31 12:58:01 +0200

    Added DTOs for posting and getting data from the api

---
+++ Assets/Scripts/Data/UserDTO.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class UserDTO : MonoBehaviour
+{
+
+    public string LapTime { get; set; }
+
+    public int Rank { get; set; }
+
+    public CarDTO Car { get; set; }
+}
--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -15,16 +15,20 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private Image progressBar;
 
+    [SerializeField]
+    private string api;
+
     private void Awake()
     {
         Instance = this;
-
+        StartCoroutine(GetData(api));
+        StartCoroutine(PostData(api));
     }
     public async void LoadScene(string sceneName)
     {
         var scene = SceneManager.LoadSceneAsync(sceneName);
         scene.allowSceneActivation = false;
-        
+
         loadingScreen.SetActive(true);
 
         do
@@ -39,63 +43,48 @@ public class GameManager : MonoBehaviour
         scene.allowSceneActivation = true;
         loadingScreen.SetActive(false);
     }
+    private IEnumerator GetData(string api)
+    {
+        UnityWebRequest request = UnityWebRequest.Get(api);
+        yield return request.SendWebRequest();
 
-    //public class ApiManager : MonoBehaviour
-    //{
-    //    private string apiUrl = "https://your-api-url/api/users"; // Replace with your actual API endpoint
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Received: " + request.downloadHandler.text);
+            // Process the received data
+        }
+        else
+        {
+            Debug.LogError("Error: " + request.error);
+        }
+    }
 
-    //    void Start()
-    //    {
-    //        StartCoroutine(GetData());
-    //    }
+    private IEnumerator PostData(string api)
+    {
+        // Create a user object to send to the API
+        UserDTO userToSend = new();
+        var currentTime = new LapTimer();
 
-    //    IEnumerator GetData()
-    //    {
-    //        UnityWebRequest request = UnityWebRequest.Get(apiUrl);
-    //        yield return request.SendWebRequest();
+        // Convert the user object to JSON
+        string jsonData = JsonUtility.ToJson(userToSend);
 
-    //        if (request.result == UnityWebRequest.Result.Success)
-    //        {
-    //            Debug.Log("Received: " + request.downloadHandler.text);
-    //            // Process the received data
-    //        }
-    //        else
-    //        {
-    //            Debug.LogError("Error: " + request.error);
-    //        }
-    //    }
+        // Set up the request
+        UnityWebRequest request = new UnityWebRequest(api, "POST");
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
 
-    //    IEnumerator PostData()
-    //    {
-    //        // Create a user object to send to the API
-    //        User userToSend = new User
-    //        {
-    //            ID = 0, // Assuming the server generates the ID
-    //            UserName = "NewUser",
-    //            // Add other properties as needed
-    //        };
+        // Send the request
+        yield return request.SendWebRequest();
 
-    //        // Convert the user object to JSON
-    //        string jsonData = JsonUtility.ToJson(userToSend);
-
-    //        // Set up the request
-    //        UnityWebRequest request = new UnityWebRequest(apiUrl, "POST");
-    //        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
-    //        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
-    //        request.downloadHandler = new DownloadHandlerBuffer();
-    //        request.SetRequestHeader("Content-Type", "application/json");
-
-    //        // Send the request
-    //        yield return request.SendWebRequest();
-
-    //        if (request.result == UnityWebRequest.Result.Success)
-    //        {
-    //            Debug.Log("Data sent successfully");
-    //        }
-    //        else
-    //        {
-    //            Debug.LogError("Error: " + request.error);
-    //        }
-    //    }
-    //}
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            Debug.Log("Data sent successfully");
+        }
+        else
+        {
+            Debug.LogError("Error: " + request.error);
+        }
+    }
 }
