95c869eb151b73bfbd27979423bdb4bec8bb9554
Момчил Инджов <momchilindzov@icloud.com>
2024-01-24 18:43:13 +0200

    Update

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
-    /// <summary>
-    /// Loads the scene which is given as a string parameter
-    /// </summary>
-    /// <param name="name"></param>
     public void ChangeScene(string name)
     {
         GameManager.Instance.LoadScene(name);
